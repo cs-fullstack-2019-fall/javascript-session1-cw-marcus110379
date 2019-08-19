69cd04d5171b2b0204e472e4996d946a6eba5b5c
marcus110379 <marcusjmadden@gmail.com>
2019-08-19 16:08:05 -0500

    cw

---
+++ index.js
@@ -0,0 +1,34 @@
+// alert('Test');
+// Create a FacebookUser class with one attribute: name. In a main function use the array below to create an array of FacebookUser objects and console.log that new array.
+//
+//     ``` javascript
+// let myArray = ['Marvin','Ferris','Brynn','Damian','Whitney','Leila','Frances','Keith','Cara','Ainsley','Adrian','Adam','Iona','Akeem','Scarlett','Murphy','Angelica','Georgia','Calvin','Amy','Merrill','Gloria','Charissa','Colorado','Inga','Eden','Carla','Hedley','Thaddeus','Maggie','Kimberly','Ezra','Kane','Candace','Cade','Dante','Scott','Philip','Dieter','Kristen','Nerea','Ivor','Stacey','Tanner','Judith','Emery','Lionel','Josiah','Wesley','Anne','Lane','Kameko','Colleen','Travis','Abdul','Scarlet','Quail','Larissa','Palmer','Irene','Cherokee','Rajah','Maxine','Yen','Bruce','Matthew','Ivan','Georgia','Charissa','Thaddeus','Jack','Dean','Florence','Madonna','Dennis','Zeus','Destiny','Maia','Mara','Florence','Anika','Brenda','Jocelyn','Zia','Kiona','Lars','Molly','Gregory','Felix','William','Dahlia','Gil','Byron','Daria','Nevada','Claudia','Zelenia','Nathan','Judah','Sheila']
+// ```
+
+//function main() {
+
+
+    class FacebookUser {
+        constructor(name) {
+            this.name = name;
+
+        }
+    }
+
+
+
+    let myArray = ['Marvin', 'Ferris', 'Brynn', 'Damian', 'Whitney', 'Leila', 'Frances', 'Keith', 'Cara', 'Ainsley', 'Adrian', 'Adam', 'Iona', 'Akeem', 'Scarlett', 'Murphy', 'Angelica', 'Georgia', 'Calvin', 'Amy', 'Merrill', 'Gloria', 'Charissa', 'Colorado', 'Inga', 'Eden', 'Carla', 'Hedley', 'Thaddeus', 'Maggie', 'Kimberly', 'Ezra', 'Kane', 'Candace', 'Cade', 'Dante', 'Scott', 'Philip', 'Dieter', 'Kristen', 'Nerea', 'Ivor', 'Stacey', 'Tanner', 'Judith', 'Emery', 'Lionel', 'Josiah', 'Wesley', 'Anne', 'Lane', 'Kameko', 'Colleen', 'Travis', 'Abdul', 'Scarlet', 'Quail', 'Larissa', 'Palmer', 'Irene', 'Cherokee', 'Rajah', 'Maxine', 'Yen', 'Bruce', 'Matthew', 'Ivan', 'Georgia', 'Charissa', 'Thaddeus', 'Jack', 'Dean', 'Florence', 'Madonna', 'Dennis', 'Zeus', 'Destiny', 'Maia', 'Mara', 'Florence', 'Anika', 'Brenda', 'Jocelyn', 'Zia', 'Kiona', 'Lars', 'Molly', 'Gregory', 'Felix', 'William', 'Dahlia', 'Gil', 'Byron', 'Daria', 'Nevada', 'Claudia', 'Zelenia', 'Nathan', 'Judah', 'Sheila'];
+
+
+
+
+     for(let i = 0; i < myArray.length; i++){
+       
+    }
+   let facebook1 = new FacebookUser("marvin");
+//    let facebook2 = new FacebookUser("ferris");
+//    let facebook3 = new FacebookUser("brynn");
+//  //console.log(facebook1);
+//
+//     let newArray = [];
+// newArray.push(facebook1);
